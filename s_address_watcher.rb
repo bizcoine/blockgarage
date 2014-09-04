686595c1c9009449727aa800d27558810c22d45d
Atif Nazir <a@block.io>
2014-09-04 14:33:43 -0700

    address watcher

---
+++ s_address_watcher.rb
@@ -0,0 +1,9 @@
+# watches Block.io addresses for balance changes using SoChain's API
+
+# create 5 wallet addresses
+
+# subscribe to realtime balance updates for all of them
+
+# alert us when the balance changes for any given address
+
+# keep going until we stop
