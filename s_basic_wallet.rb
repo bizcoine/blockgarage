d07745d671a1f3086aa69a4f2e5b2b7e06b4d2d8
Atif Nazir <a@block.io>
2014-09-04 15:22:12 -0700

    revised skeletons

--- s_basic_wallet.rb
+++ s_basic_wallet.rb
@@ -1,7 +1,13 @@
 # create some wallet addresses, fund them with some coins using Block.io's API
 
-# waits for it to get funds (by polling Block.io's API)
+# create an address, label=demo1
 
-# forwards the funds to a designated address
+# fund it with some coins from label=default (testnet)
+
+# wait for it to get funds (by polling Block.io's API)
+
+# create an address label=demo2
+
+# forward the funds to a demo2
 
 # exit
